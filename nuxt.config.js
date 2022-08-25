a5eb837923eb3b0df027d697ecb35e9624da3ad1
DanielMarlin <daniel@pascalfurniture.com>
2022-08-25 15:21:41 +0200

    Refactored generate code

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,12 +22,7 @@ export default {
     async ready() {
       const { $content } = require('@nuxt/content')
       const articles = await $content().only(['slug', 'clusterPath']).fetch()
-      return articles.map(x => {
-        return {
-          route: `${x.clusterPath}/${x.slug}`,
-          payload: x
-        }
-      })
+      return articles.map(x => (`${x.clusterPath}/${x.slug}`))
     },
     fallback: '404.html'
   },
